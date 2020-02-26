948aff066b18192266b2135038e1f2e49b5ddacf
jsantana29 <santanarijo29@gmail.com>
2020-02-25 19:07:46 -0500

    Changing flashlight range
    Adjusted the toggle flashlight script so that it increases/decreases the range instead of intensity

--- Kimodameshi/Assets/Scripts/Flashlight/ToggleFlashlight.cs
+++ Kimodameshi/Assets/Scripts/Flashlight/ToggleFlashlight.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:642f523008a7ae658eefe4229fc62105afb87b8b832c6e7700558f9879cbc97b
-size 1346
+oid sha256:f1aa92ce4b9f1c34beba67b10546550923d34356c42ed2fab16f8eeee3c91bfd
+size 1348
