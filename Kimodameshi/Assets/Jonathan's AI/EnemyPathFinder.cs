3240eb409bc9b3739e300312d9ed42ac59db5ff7
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2020-03-10 22:15:37 -0400

    New scripts and update to files

--- Kimodameshi/Assets/Jonathan's AI/EnemyPathFinder.cs
+++ Kimodameshi/Assets/Jonathan's AI/EnemyPathFinder.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3010b3913bc1285c147293c71becb612f6c0ebaeedbf35052927384cddcadf3b
-size 4545
+oid sha256:efbef5e046a3e6f4cf0f7239057088963cb9f91d1e2f54b902b859c2a171c0ab
+size 4537
---
+++ Kimodameshi/Assets/Jonathan's AI/IInteractable.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9beecae38ead2c5df38a627feeef641bc4847b757cb73ba24175e818fc69a49e
+size 202
